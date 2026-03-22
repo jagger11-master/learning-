21fbb2e8c9395f8b92698c1ab03dc6c3f5f5088b
esam <emmanueldaud358@gmail.com>
2026-03-22 17:48:57 +0300

    logic.js

---
+++ logic.js
@@ -0,0 +1,15 @@
+let x = 16;
+let name = "me";
+let text = "you can not drive";
+
+if(name == "me"){
+    if( x != 16) {
+    text = "you are not eligible to drink";
+}
+else{
+    text = "you are eligible to drink";
+}}
+else{
+    text = "null";
+}
+console.log(text);
---
+++ pulse.js
@@ -0,0 +1,30 @@
+if (condition1){
+    //code block to be excuted this specify the condition if it id true 
+}
+else if (condition2) {
+    //the code statement to check if condtion 1 is false and condtion 2 is true 
+}
+else{
+    //the code to be excuted if is false
+}
+
+// trying using another simple case for managing if.... else statement 
+
+switch(expression) {
+    case 1:
+        //code block
+        break;
+    case 2:
+        //code block
+        break;
+        
+    case 3:
+        //code block
+        break;
+    default:
+        //code block    
+}
+
+// the best alternative for if...else statement using the shorthand(?..:)
+
+condtion1 ? expression1 : expression2
